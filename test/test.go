2044324684c1851b4c2fb4ba730a15d325f52dc7
KimSungJin <rainbow96bear@gmail.com>
2023-08-24 01:12:12 +0900

    append를 이용한 요소 제거 의문점

--- test/test.go
+++ test/test.go
@@ -2,8 +2,10 @@ package main
 
 import "fmt"
 
-func main(){
-	a:=10
-	fmt.Println("a: ",a)
 
+func main(){
+	checkArr1 := []int{1, 3, 5, 7, 9, 11}
+	checkArr2 := append(checkArr1[:3],checkArr1[4:]...)
+	fmt.Println(checkArr2)
+	fmt.Println(checkArr1)
 }
