bc15f99bbee6505edcdae3a149ed19fd8b5255e6
KimSungJin <rainbow96bear@gmail.com>
2023-08-28 11:28:34 +0900

    goroutine test code

--- testcode/testcode.go
+++ testcode/testcode.go
@@ -10,15 +10,16 @@ var wg sync.WaitGroup
 func testFunc(s string){
 	for i:=0; i<5 ; i++{
 		fmt.Println(s,i+1)
-		time.Sleep(100*time.Millisecond)
+		// time.Sleep(100*time.Millisecond)
 	}
 }
 func main(){
-	wg.Add(3)
+	// wg.Add(3)
 	testFunc("Sync")
 
 	go testFunc("Async1")
 	go testFunc("Async2")
 	go testFunc("Async3")
-	wg.Wait()	
+	// wg.Wait()
+	time.Sleep(10*time.Second)	
 }
