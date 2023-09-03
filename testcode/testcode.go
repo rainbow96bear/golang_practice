a365c692d9d6e4e79818a1929e061d3e14f2be76
KimSungJin <rainbow96bear@gmail.com>
2023-09-03 15:18:28 +0900

    Fscanln과 Fscan 차이 test

--- testcode/testcode.go
+++ testcode/testcode.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"sync"
-	"time"
+	"os"
 )
 
-var wg sync.WaitGroup
-func testFunc(s string){
-	for i:=0; i<5 ; i++{
-		fmt.Println(s,i+1)
-		// time.Sleep(100*time.Millisecond)
-	}
-}
-func main(){
-	// wg.Add(3)
-	testFunc("Sync")
 
-	go testFunc("Async1")
-	go testFunc("Async2")
-	go testFunc("Async3")
-	// wg.Wait()
-	time.Sleep(10*time.Second)	
+func main(){
+	rx := bufio.NewReader(os.Stdin)
+	wx := bufio.NewWriter(os.Stdout)
+	defer wx.Flush()
+
+	var testNum int
+
+	fmt.Fscanln(rx, &testNum)
+	var x1, y1, x2, y2 int
+	fmt.Fscanln(rx, &x1, &y1, &x2, &y2)
+	var planetsNum int
+	fmt.Fscanln(rx,&planetsNum)
+	
+	fmt.Println(testNum)
+	fmt.Println(x1, y1, x2, y2)
+	fmt.Println(planetsNum)
 }
