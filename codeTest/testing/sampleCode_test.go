252afb70107cc74767f70fb8906e18cfc6993c1a
KimSungJin <rainbow96bear@gmail.com>
2023-09-13 17:58:35 +0900

    testcase 추가

--- codeTest/testing/sampleCode_test.go
+++ codeTest/testing/sampleCode_test.go
@@ -2,9 +2,21 @@ package main
 
 import "testing"
 
-func TestAdd(t *testing.T) {
+func TestAdd1(t *testing.T) {
+	result := Add(4, 7)
+	expected1 := 11
+	expected2 := 13
+	if result != expected1 {
+		t.Errorf("Expected %d but got %d", expected1, result)
+	}
+	if result != expected2 {
+		t.Errorf("Expected %d but got %d", expected2, result)
+	}
+}
+func TestAdd2(t *testing.T) {
 	result := Add(4, 7)
 	expected := 11
+	
 	if result != expected {
 		t.Errorf("Expected %d but got %d", expected, result)
 	}
